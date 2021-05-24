ca465f2e9d661c16ff087905436d9d8a24175dbe
Jain-sonal <sonaljain.mcs@gmail.com>
2021-05-24 20:45:38 +0530

    Include Particle.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import Particles from 'node_modules/particlesjs/dist/particles.min.js';
 var particles = Particles.init({
 	selector: '.background',
     color: ['#03045E','#023E8A','#0077B6','#0096C7','#00B4D8','#48CAE4','#90E0EF','#ADE8F4','#CAF0F8'],
